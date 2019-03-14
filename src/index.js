49c3f7de477913e12d3a8f315f7fd7f9e21afe0e
p-12s <mental.notes@yandex.ru>
2019-03-15 00:12:08 +0300

    Изменил обертывающий див на React.Fragment, чтобы не плодить бесполезные контейнеры

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MarkedList from './MarkedList';
+import FunctionalComponent from './FunctionalComponent';
+import MyInfo from './MyInfo';
 
 //ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
-ReactDOM.render(<div><App/><MarkedList/></div>, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+        <App/>
+        <MarkedList/>
+        <FunctionalComponent/>
+        <MyInfo/>
+    </React.Fragment>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
