216f17ad1f0334d54ff51972de0c9c944222f760
Pavel V <mental.notes@yandex.ru>
2019-05-18 15:40:38 +0300

    // my new comment in dev (#2)

--- src/App.js
+++ src/App.js
@@ -84,6 +84,7 @@ class App extends React.Component {
     }
 
     getSnapshotBeforeUpdate() {
+        // new comment dev
         // my new comment in feature/N-1
         // my new comment 2 in feature/N-1
         // this is squoshed comments
